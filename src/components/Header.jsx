b9e5feb59fe8817377a473d848ee8ed5b20d7924
siotan <nyaa.-525@outlook.jp>
2023-08-28 07:53:23 +0900

    responsive Home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,32 +6,32 @@ import { AiFillGithub } from "react-icons/ai";
 
 export const Header = () => {
   return (
-    <header className=" p-2 text-center space-y-1">
-      <p className="text-red-300">Highblo</p>
-      <ul className="flex justify-center gap-2">
+    <header className="p-2 text-center space-y-1">
+      <p className="text-red-400">Highblo@2023</p>
+      <ul className="flex justify-center gap-6">
         <li>
           <a href="#">
-            <BsInstagram color="#eb7e63" size={22} />
+            <BsInstagram color="#eb7e63" className="text-2xl md:text-4xl" />
           </a>
         </li>
         <li>
           <a href="#">
-            <FaTwitterSquare color="#1da1f2" size={22} />
+            <FaTwitterSquare color="#1da1f2" className="text-2xl md:text-4xl" />
           </a>
         </li>
         <li>
           <a href="#">
-            <AiFillYoutube color="red" size={22} />
+            <AiFillYoutube color="red" className="text-2xl md:text-4xl" />
           </a>
         </li>
         <li>
           <a href="#">
-            <SiQiita color="#55c500" size={22} />
+            <SiQiita color="#55c500" className="text-2xl md:text-4xl" />
           </a>
         </li>
         <li>
           <a href="https://github.com/Highblo">
-            <AiFillGithub color="black" size={22} />
+            <AiFillGithub color="black" className="text-2xl md:text-4xl" />
           </a>
         </li>
       </ul>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     location.href = "https://haru-portfolio71.netlify.app/";
   }, []);
 
-  const onClickContact = useCallback(() => {
+  const onClickCode = useCallback(() => {
     location.href = "https://github.com/Highblo/Highblo-Next";
   }, []);
 
@@ -16,7 +16,7 @@ const Home = () => {
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ["fullstack developer", "frontend engineer", "freedom lover"],
+      strings: ["fullstack engineer", "frontend engineer", "freedom lover"],
       typeSpeed: 50,
       backSpeed: 40,
       loop: true,
@@ -29,30 +29,36 @@ const Home = () => {
   }, []);
 
   return (
-    <section className="w-[92%] mx-auto mt-10 px-2">
-      <div className=" space-y-6">
-        <h1 className="text-6xl stroke text-transparent">
+    <section className="w-[92%] mx-auto mt-8 px-2 sm:mt-14 md:text-center">
+      <div className=" space-y-4 sm:space-y-6">
+        <h1 className="text-6xl stroke text-transparent sm:text-7xl md:text-8xl">
           Hello
           <br />
           I'm Highblo
         </h1>
-        <h2 className="text-2xl">
-          I'm a <span className="text-red-400 text-3xl" ref={el}></span>
+        <h2 className="text-2xl sm:text-3xl">
+          I'm a{" "}
+          <span
+            className="text-gradient text-3xl font-bold sm:text-4xl md:text-5xl"
+            ref={el}
+          ></span>
         </h2>
-        <p>
+      </div>
+      <div className="mt-6 sm:mt-10 lg:w-[70%] lg:mx-auto">
+        <p className="mb-8 md:mb-12 md:text-lg">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id tempora
           enim eaque nobis omnis eum tempore, quibusdam suscipit! Nam sit
           voluptates fugit ducimus labore quo. Magni repudiandae eius velit
           sequi.
         </p>
-      </div>
-      <div className="space-y-4 mt-10">
-        <Button text="text-white" bg="bg-gray-700" onClick={onClickOldSite}>
-          Old site
-        </Button>
-        <Button text="text-gray-700" bg="bg-white" onClick={onClickContact}>
-          Source Code
-        </Button>
+        <div className="space-y-6 md:w-[80%] md:mx-auto">
+          <Button text="text-white" bg="bg-gray-700" onClick={onClickOldSite}>
+            Old site
+          </Button>
+          <Button text="text-gray-700" bg="bg-white" onClick={onClickCode}>
+            Source Code
+          </Button>
+        </div>
       </div>
     </section>
   );
