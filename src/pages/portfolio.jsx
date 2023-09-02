89226885b9b1c9e1f79d122487dda26fe55eaa6d
siotan <nyaa.-525@outlook.jp>
2023-09-02 14:14:56 +0900

    portfolio

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,5 +1,39 @@
+import { DammyPortfolio } from "@/components/DammyPortfolio";
+import { SectionTitle } from "@/components/SectionTitle";
+
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section className="mt-3">
+      <SectionTitle>Portfolio</SectionTitle>
+      <p className="text-center mb-1">随時追加する予定です</p>
+      <div className="w-[80%] mx-auto h-[56vh] overflow-y-scroll p-2 space-y-4 md:space-y-0 md:flex md:flex-wrap md:justify-around md:items-center">
+        <DammyPortfolio
+          bgGradient="bg-gradient1"
+          hoverBgGradient="hover:scale-100"
+        />
+        <DammyPortfolio
+          bgGradient="bg-gradient2"
+          hoverBgGradient="hover:scale-100"
+        />
+        <DammyPortfolio
+          bgGradient="bg-gradient1"
+          hoverBgGradient="hover:scale-100"
+        />
+        <DammyPortfolio
+          bgGradient="bg-gradient2"
+          hoverBgGradient="hover:scale-100"
+        />
+        <DammyPortfolio
+          bgGradient="bg-gradient1"
+          hoverBgGradient="hover:scale-100"
+        />
+        <DammyPortfolio
+          bgGradient="bg-gradient2"
+          hoverBgGradient="hover:scale-100"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
